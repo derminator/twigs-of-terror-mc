15c88ec46813fb92c992fcbf035ba331bc3c33b0
Michael <michaelzd95@gmail.com>
2025-07-12 16:18:46 -0400

    update comment

--- src/main/kotlin/com/github/derminator/derminatorsMcMod/DerminatorsMcMod.kt
+++ src/main/kotlin/com/github/derminator/derminatorsMcMod/DerminatorsMcMod.kt
@@ -4,8 +4,6 @@ import net.neoforged.fml.common.Mod
 
 /**
  * Main mod class.
- *
- * An example for blocks is in the `blocks` package of this mod.
  */
 @Mod(DerminatorsMcMod.ID)
 object DerminatorsMcMod {
