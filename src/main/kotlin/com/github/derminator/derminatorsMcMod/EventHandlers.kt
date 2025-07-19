1d6a0660ccd719c77947e3c3f54462bac9d98c5c
Michael <michaelzd95@gmail.com>
2025-07-19 16:54:26 -0400

    give random player a stick every 100 ticks

--- src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
+++ src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
@@ -1,11 +1,21 @@
 package com.github.derminator.derminatorsMcMod
 
 import net.neoforged.bus.api.SubscribeEvent
-import net.neoforged.neoforge.event.entity.player.PlayerEvent
+import net.neoforged.neoforge.event.tick.ServerTickEvent
 
 object EventHandlers {
+    val playersNeedingStick = mutableMapOf<String, Int>()
+
     @SubscribeEvent
-    fun onPlayerJoin(event: PlayerEvent.PlayerLoggedInEvent) {
-        giveStick(event.entity)
+    fun onServerTick(event: ServerTickEvent.Post) {
+        val players = event.server.playerList.players
+        if (!players.isEmpty()) {
+            val player = players.random()
+            val stickTime = playersNeedingStick[player.name.string] ?: 0
+            if (stickTime <= event.server.tickCount) {
+                giveStick(player)
+                playersNeedingStick[player.name.string] = event.server.tickCount + 100
+            }
+        }
     }
 }
