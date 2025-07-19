4e6fd32676e277b1e347ac861d257ae07de4c37a
Michael <michaelzd95@gmail.com>
2025-07-18 20:31:40 -0400

    refactor: extract stick-giving logic into `ServerActions.giveStick` and reuse in `EventHandlers`

--- src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
+++ src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
@@ -1,20 +1,11 @@
 package com.github.derminator.derminatorsMcMod
 
-import net.minecraft.network.chat.Component
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.neoforge.event.entity.player.PlayerEvent
 
 object EventHandlers {
     @SubscribeEvent
     fun onPlayerJoin(event: PlayerEvent.PlayerLoggedInEvent) {
-        val player = event.entity
-
-        // Send welcome message
-        player.sendSystemMessage(Component.literal("Welcome to the server; here's a stick!"))
-
-        // Add a stick to player's inventory
-        player.inventory.add(ItemStack(Items.STICK))
+        giveStick(event.entity)
     }
 }
