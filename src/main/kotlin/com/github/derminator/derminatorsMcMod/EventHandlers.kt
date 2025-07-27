6dbb4d367b75346ee2d2eac97e3a09f281d89e18
Michael <michaelzd95@gmail.com>
2025-07-27 14:14:50 -0400

    randomly change stick damage

--- src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
+++ src/main/kotlin/com/github/derminator/derminatorsMcMod/EventHandlers.kt
@@ -1,7 +1,14 @@
 package com.github.derminator.derminatorsMcMod
 
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.Items
 import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent
 import net.neoforged.neoforge.event.tick.ServerTickEvent
+
+private const val MIN_DAMAGE_PROB = 0.001
+private const val MAX_DAMAGE_PROB = 0.01
+private const val ADJUSTED_DAMAGE = 9999.9f
 
 object EventHandlers {
     @SubscribeEvent
@@ -10,6 +17,18 @@ object EventHandlers {
         if (!players.isEmpty()) {
             val player = players.random()
             giveStickIfNeeded(player)
+        }
+    }
+
+    @SubscribeEvent
+    fun onLivingDamage(event: LivingDamageEvent.Pre) {
+        val source = event.source.entity
+        if (event.source.weaponItem == Items.STICK && source is Player) {
+            val prob =
+                MIN_DAMAGE_PROB + (MAX_DAMAGE_PROB - MIN_DAMAGE_PROB) * getUserEventProbability(source.name.string)
+            if (Math.random() < prob) {
+                event.newDamage = ADJUSTED_DAMAGE
+            }
         }
     }
 }
